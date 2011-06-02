76f91fa6a3c642b857c380717dd8658c5daafddb
Rob Hoelz <rob@hoelz.ro>
2011-06-02 10:50:23 -0500

    Initial commit of Dist::Zilla config

---
+++ profiles/default/Module.pm
@@ -0,0 +1,20 @@
+package {{$name}};
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A brief description of your module goes here
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
